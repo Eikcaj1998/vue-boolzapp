5ee1d1b7aeab2f11fa305c76722143ab8b6b17dd
Eikcaj.1998 <jw220998@gmail.com>
2022-07-15 15:26:41 +0200

    aggiunto parte dell esercizio

--- js/script.js
+++ js/script.js
@@ -19,8 +19,9 @@ Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono
 visualizzati solo i contatti il cui nome contiene le lettere 
 inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono 
   solo Marco e Martina) */
-
+  
   console.log('js ok',Vue)
+  dayjs.extend(dayjs_plugin_customParseFormat);
 
   const app = new Vue(
     {
@@ -30,44 +31,158 @@ inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono
           name: 'MMDita',
           avatar: '_io',
         },
-      contacts: [
-        {
-          name: 'Marco',
-          avatar: '_1',
-        },
-        {
-          name: 'Giorgia',
-          avatar: '_2',
-        },
-        {
-          name: 'Fabrizio',
-          avatar: '_3',
-        },
-        {
-          name: 'Gabriele',
-          avatar: '_4',
-        },
-        {
-          name: 'Gianluca',
-          avatar: '_5',
-        },
-        {
-          name: 'Andrei',
-          avatar: '_6',
-        },
-        {
-          name: 'Federico',
-          avatar: '_7',
-        },
-        {
-          name: 'Alfredo',
-          avatar: '_8',
-        },
-    ]
+    contacts: [
+  {
+    name: 'Marco',
+    avatar: '_1',
+    visible: true,
+    messages: [{
+      date: '10/01/2020 15:30:55',
+      text: 'Hai portato a spasso il cane?',
+      status: 'sent'
+    },
+    {
+      date: '10/01/2020 15:50:00',
+      text: 'Ricordati di dargli da mangiare',
+      status: 'sent'
+    },
+    {
+      date: '10/01/2020 16:15:22',
+      text: 'Tutto fatto!',
+      status: 'received'
+    }
+    ],
+  },
+  {
+    name: 'Giorgia',
+    avatar: '_2',
+    visible: true,
+    messages: [{
+      date: '20/03/2020 16:30:00',
+      text: 'Ciao come stai?',
+      status: 'sent'
+    },
+    {
+      date: '20/03/2020 16:30:55',
+      text: 'Bene grazie! Stasera ci vediamo?',
+      status: 'received'
+    },
+    {
+      date: '20/03/2020 16:35:00',
+      text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+      status: 'received'
+    }
+    ],
+  },
+  {
+    name: 'Fabrizio',
+    avatar: '_3',
+    visible: true,
+    messages: [{
+      date: '28/03/2020 10:10:40',
+      text: 'La Marianna va in campagna',
+      status: 'received'
+    },
+    {
+      date: '28/03/2020 10:20:10',
+      text: 'Sicuro di non aver sbagliato chat?',
+      status: 'sent'
+    },
+    {
+      date: '28/03/2020 16:15:22',
+      text: 'Ah scusa!',
+      status: 'received'
+    }
+    ],
+  },
+  {
+    name: 'Gabriele',
+    avatar: '_4',
+    visible: true,
+    messages: [{
+      date: '10/01/2020 15:30:55',
+      text: 'Lo sai che ha aperto una nuova pizzeria?',
+      status: 'sent'
+    },
+    {
+      date: '10/01/2020 15:50:00',
+      text: 'Si, ma preferirei andare al cinema',
+      status: 'received'
+    }
+    ],
+  },
+  {
+    name: 'Gianluca',
+    avatar: '_5',
+    visible: true,
+    messages: [{
+      date: '10/01/2020 15:30:55',
+      text: 'Lo sai che ha aperto una nuova pizzeria?',
+      status: 'sent'
+    },
+    {
+      date: '10/01/2020 15:50:00',
+      text: 'Si, ma preferirei andare al cinema',
+      status: 'received'
+    }
+    ],
+  },
+  {
+    name: 'Andrei',
+    avatar: '_6',
+    visible: true,
+    messages: [{
+      date: '10/01/2020 15:30:55',
+      text: 'Lo sai che ha aperto una nuova pizzeria?',
+      status: 'sent'
+    },
+    {
+      date: '10/01/2020 15:50:00',
+      text: 'Si, ma preferirei andare al cinema',
+      status: 'received'
+    }
+    ],
+  },
+  {
+    name: 'Federico',
+    avatar: '_7',
+    visible: true,
+    messages: [{
+      date: '10/01/2020 15:30:55',
+      text: 'Lo sai che ha aperto una nuova pizzeria?',
+      status: 'sent'
+    },
+    {
+      date: '10/01/2020 15:50:00',
+      text: 'Si, ma preferirei andare al cinema',
+      status: 'received'
+    }
+    ],
+  },
+  {
+    name: 'Alfredo',
+    avatar: '_8',
+    visible: true,
+    messages: [{
+      date: '10/01/2020 15:30:55',
+      text: 'Lo sai che ha aperto una nuova pizzeria?',
+      status: 'sent'
+    },
+    {
+      date: '10/01/2020 15:50:00',
+      text: 'Si, ma preferirei andare al cinema',
+      status: 'received'
+    }
+    ],
+  },
+]
 },
   methods:{
     getContacts(index){
     return 'img/avatar' + this.contacts[index].avatar + '.jpg';
 },
+    printDate(){
+      return dayjs(this.birthday).format('DD/MM/YYYY HH/mm/ss')
+    }
 }
 })
